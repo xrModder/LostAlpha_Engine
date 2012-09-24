a79d39ca7578a19a6e2b1219ef19404f7a26ddf4
kolmogor <kolmogor200@gmail.com>
2012-09-24 14:20:01 +0400

    make shadows right length, depending from sun position

--- xray-svn-trunk/xr_3da/xrRender/Light_DB.cpp
+++ xray-svn-trunk/xr_3da/xrRender/Light_DB.cpp
@@ -183,6 +183,10 @@ void			CLight_DB::Update			()
 		sun_adapted->set_position	(AP		);
 		sun_adapted->set_color		(E.sun_color.x*ps_r2_sun_lumscale,E.sun_color.y*ps_r2_sun_lumscale,E.sun_color.z*ps_r2_sun_lumscale);
 		sun_adapted->set_range		(600.f	);
+
+		//test: take from CoP for real shadows from sun
+		sun_adapted->set_rotation (OD,_sun_original->right	);
+		sun_adapted->set_position (OP);
 	}
 
 	// Clear selection
