fa53416cde81ccb150cf2aacad907a84811a8c2c
SkyLoader <skyloader1@mail.ru>
2013-01-17 15:15:22 +0400

    fixed script callbacks for quicksave, quickload (task #6)

--- xray-svn-trunk/xr_3da/xrGame/console_commands.cpp
+++ xray-svn-trunk/xr_3da/xrGame/console_commands.cpp
@@ -482,11 +482,14 @@ public:
 			return;
 		}
 
-		string256					func_name;
-		luabind::functor<LPCSTR>			func;
-		strcpy_s(func_name,pSettings->r_string("lost_alpha_cfg","quicksave_function"));
-		R_ASSERT					(ai().script_engine().functor<LPCSTR>(func_name,func));
-		func						();
+		if (pSettings->section_exist("lost_alpha_cfg") && pSettings->line_exist("lost_alpha_cfg","on_save_callback"))
+		{
+			string256					func_name;
+			luabind::functor<LPCSTR>			func;
+			strcpy_s(func_name,pSettings->r_string("lost_alpha_cfg","on_save_callback"));
+			R_ASSERT					(ai().script_engine().functor<LPCSTR>(func_name,func));
+			func						();
+		}
 
 		string_path				S,S1;
 		S[0]					= 0;
@@ -568,11 +571,14 @@ public:
 			return;
 		}
 
-		string256					func_name;
-		luabind::functor<LPCSTR>			func;
-		strcpy_s(func_name,pSettings->r_string("lost_alpha_cfg","quickload_function"));
-		R_ASSERT					(ai().script_engine().functor<LPCSTR>(func_name,func));
-		func						();
+		if (pSettings->section_exist("lost_alpha_cfg") && pSettings->line_exist("lost_alpha_cfg","on_load_callback"))
+		{
+			string256					func_name;
+			luabind::functor<LPCSTR>			func;
+			strcpy_s(func_name,pSettings->r_string("lost_alpha_cfg","on_load_callback"));
+			R_ASSERT					(ai().script_engine().functor<LPCSTR>(func_name,func));
+			func						();
+		}
 
 /*     moved to level_network_messages.cpp
 		CSavedGameWrapper			wrapper(args);
