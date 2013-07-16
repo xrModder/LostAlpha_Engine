87ccb920bf2d1d4e1450cb894f283cf73d8f2a74
SkyLoader <skyloader1@mail.ru>
2013-07-17 00:32:03 +0400

    added switching actor_state_icon.xml in 'ui_hud_type'

--- xray-svn-trunk/xr_3da/xrGame/UIActorStateIcons.cpp
+++ xray-svn-trunk/xr_3da/xrGame/UIActorStateIcons.cpp
@@ -1,8 +1,7 @@
 #include "stdafx.h"
 #include "UIActorStateIcons.h"
 #include "UI/UIXmlInit.h"
-
-const LPCSTR STATE_ICONS_XML = "actor_state_icon.xml";
+#include "hudmanager.h"
 
 CUIActorStateIcons::CUIActorStateIcons()
 {
@@ -22,6 +21,10 @@ CUIActorStateIcons::~CUIActorStateIcons()
 void CUIActorStateIcons::Init()
 {
 	CUIXml		xml;
+
+	string128		STATE_ICONS_XML;
+	sprintf_s		(STATE_ICONS_XML, "actor_state_icon_%d.xml", ui_hud_type);
+
 	bool		result			= xml.Init			(CONFIG_PATH, UI_PATH, STATE_ICONS_XML);
 	R_ASSERT3										(result, "xml file not found", STATE_ICONS_XML);
 
