d1256058529c97862fcc2278adca4992550e28b6
gr1ph00n <gr1ph00n@gmail.com>
2013-08-17 19:27:41 +0200

    added some debug info for sounds comments and cars only in mixed mode

--- xray-svn-trunk/xrSound/SoundRender_Source_loader.cpp
+++ xray-svn-trunk/xrSound/SoundRender_Source_loader.cpp
@@ -116,7 +116,10 @@ void CSoundRender_Source::LoadWave	(LPCSTR pName)
 #endif
 	}
 	R_ASSERT3((m_fMaxAIDist>=0.1f)&&(m_fMaxDist>=0.1f),"Invalid max distance.",pName);
-
+#ifdef DEBUG
+	Msg("Ogg-comments loaded: [%s] :: min=[%.3f], max=[%.3f], base=[%.3f], type=[%d], maxAI=[%.2f]", 
+			pName, m_fMinDist, m_fMaxDist, m_fBaseVolume, m_uGameType, m_fMaxAIDist);
+#endif
 	ov_clear				(&ovf);
 	FS.r_close				(wave);
 }
