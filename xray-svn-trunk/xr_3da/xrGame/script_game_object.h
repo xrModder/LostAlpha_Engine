245a696de8a85799d49208da78e04211a2ef46fe
SkyLoader <skyloader1@mail.ru>
2014-01-04 18:16:43 +0300

    added 'set_actor_camera_vectors', 'set_npc_position'; fixed camera shaking if called actor:set_actor_look_at_position(pos, 0)

--- xray-svn-trunk/xr_3da/xrGame/script_game_object.h
+++ xray-svn-trunk/xr_3da/xrGame/script_game_object.h
@@ -215,6 +215,8 @@ public:
 			void				SetActorPosition	(Fvector pos);
 			void				SetActorDirection	(float dir);
 			void				SetActorDirectionVector	(Fvector dir);
+			void				SetNpcPosition		(Fvector pos);
+			void				SetActorCamActive	(Fvector pos, Fvector dir, Fvector norm);
 			void				SetActorDirectionSlowly	(Fvector pos, float time);
 			void				SetActorLegsVisible	(bool val);
 			void				SetActorCam		(u32 val);
