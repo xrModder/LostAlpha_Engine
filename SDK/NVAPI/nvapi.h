4b9dcb4b3fd5418d56e8b53905e1a448b25a7f0b
loxotron <loxotron@bk.ru>
2014-02-03 11:14:53 +0400

    let's get rid of some really annoying warnings

--- SDK/NVAPI/nvapi.h
+++ SDK/NVAPI/nvapi.h
@@ -71,7 +71,9 @@ typedef unsigned long    NvU32;
 typedef long             NvS32;
 typedef unsigned char    NvU8;
 
-#define DECLARE_HANDLE(name) struct name##__ { int unused; }; typedef struct name##__ *name
+#if (_MSC_VER <= 1500)
+	#define DECLARE_HANDLE(name) struct name##__ { int unused; }; typedef struct name##__ *name
+#endif
 
 // NVAPI Handles - These handles are retrieved from various calls and passed in to others in NvAPI
 //                 These are meant to be opaque types.  Do not assume they correspond to indices, HDCs,
--- xray-svn-trunk/BugTrap/Encoding.h
+++ xray-svn-trunk/BugTrap/Encoding.h
@@ -17,6 +17,7 @@
 #include "Stream.h"
 #include "StrStream.h"
 
+#if (_MSC_VER <= 1500)
 /// Lower value of Unicode high-surrogate character.
 #define HIGH_SURROGATE_START   0xD800
 /// Higher value of Unicode high-surrogate character.
@@ -25,6 +26,7 @@
 #define LOW_SURROGATE_START    0xDC00
 /// Higher value of Unicode low-surrogate character.
 #define LOW_SURROGATE_END      0xDFFF
+#endif
 
 /// Text encoding.
 enum TEXT_ENCODING
