c2c8c3edc3159fda36d2c1cc3935466821e0ac46
utak3r <piotr.borys@orad.pl>
2014-04-29 11:34:48 +0200

    Installer: prepare_archives script fixed, due to multivolume bug

--- installer/prepare_archives.cmd
+++ installer/prepare_archives.cmd
@@ -8,15 +8,15 @@ mkdir %OUTDIR%\game
 
 cd %GAMEDIR%\appdata
 %MAINDIR%\7za a -p%PASSWD% -r -y -v%VOLUMELIMIT% appdata.7z .
-move /y appdata.7z %OUTDIR%\game
+move /y appdata.7z* %OUTDIR%\game
 cd %GAMEDIR%\bins
 %MAINDIR%\7za a -p%PASSWD% -r -y -v%VOLUMELIMIT% bins.7z .
-move /y bins.7z %OUTDIR%\game
+move /y bins.7z* %OUTDIR%\game
 cd %GAMEDIR%\gamedata
 %MAINDIR%\7za a -p%PASSWD% -r -y -v%VOLUMELIMIT% gamedata.7z .
-move /y gamedata.7z %OUTDIR%\game
+move /y gamedata.7z* %OUTDIR%\game
 cd %GAMEDIR%
 %MAINDIR%\7za a -p%PASSWD% -r -y -v%VOLUMELIMIT% maindir.7z start.bat gamedata.db* fsgame.ltx
-move /y maindir.7z %OUTDIR%\game
+move /y maindir.7z* %OUTDIR%\game
 
 cd %MAINDIR%
